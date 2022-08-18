faeba12fc8c1af6bc6e904b93f6a93a7f5d3868a
Sergio <sergioruza22@gmail.com>
2022-08-18 15:10:55 -0300

    Requisito três, ok

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Title from './Title';
 
 class SolarSystem extends React.Component {
   render() {
+    // const teste = '';
     return (
-      <div data-testid="solar-system" />
+      <div data-testid="solar-system">
+        <Title />
+      </div>
     );
   }
 }
